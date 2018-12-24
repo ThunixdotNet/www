81cff65a630cd831209cfd59b8c816502a352e22
Anton McClure <amcclure@ttm.sh>
2018-12-24 18:42:18 +0000

    Restored original Terms of Service.

--- tos.php
+++ tos.php
@@ -1,15 +1,26 @@
 <?php include 'HEADER.php';?>
 <title>Terms of Service and Privacy Policy - Thunix Shell Provider and Web Host</title>
 <h2>Terms of Service and Privacy Policy</h2>
-<p>
-<ul>
-	<li>tl;dr - Be excellent to one another.</li>
-	<li>Thunix is a free service, with no guarantees of uptime for the various service provided.</li>
-	<li>One of the goals for Thunix is to be a collaborative environment.  Things that are detrimental to collaboration are not allowed.</li>
-	<li>Harassment of other users is not allowed.  If a user tells you to break contact, that is what you MUST do.</li>
-	<li>Be wary of rousing a sysadmin's wrath, for they are subtle, and quick to anger.  In other words, this is a fun place, and don't make it not so for the admin team.</li>
-	<li>If you have questions, ask!  Nobody gets angry over a question asked, and if they do, tough.</li>
-	<li>Don't hack the gibson.  While penetration testing can be fun, this is not the place for that.</li>
-</ul>
+<p>Nothing is without its rules &amp; regulations; thunix is no exception. Below are the site's service terms &amp; privacy policy. Everything in this page should be clear to everyone who wishes to use thunix's services, especially the service terms; failure to abide by them can result in such penalties as service bans &amp; even legal action, depending on the severity of a violation. Be sure to read the terms carefully &amp; understand them.</p>
+<h3>Terms of Service</h3>
+<p>Thunix relies on the common courtesy and consideration of users to make its services fair for everyone. The following offences will result in a service ban:</p>
+<ol>
+<li>Deliberately defacing the accounts of other users</li>
+<li>Deliberately trying to disrupt thunix's server</li>
+<li>Using thunix as a launch pad for disrupting other servers</li>
+<li>Using thunix to impersonate other websites and businesses for criminal purposes</li>
+<li>Storing/distributing pornography of any genre and medium (especially child pornography)</li>
+<li>Storing/distributing content that defames any individual</li>
+<li>Promoting racial, ethnic, religious, political &amp; other forms of bigotry</li>
+<li>Mining cryptocurrencies</li>
+</ol>
+<p>Items 4, 5, 6 &amp; 7 may also result in legal action against any offending users, depending on the severity of each case and any other individuals who may have been affected.</p>
+<p>Copyright infringement is not allowed on thunix, and we will not allow any illegal content to be distributed.</p>
+<p>It's also expected that all users respect the copyrights of those who produce original content of any kind (only share if the author or license grants you permission). However, using accounts for external file storage is permissible, under fair use &amp; other laws promoting consumer rights. Note that original content from other users must also be respected, though creating/sharing public domain content is highly encouraged.</p>
+<p>If you want to request for content you own the copyright for to be removed from thunix, please email <a href="mailto:amcclure@ttm.sh"><code>&lt;amcclure@ttm.sh&gt;</code></a>.
+In the email, please identify who you are, if you are the copyright holder or legally representing them, the exact files you want removed with links to the files and infringed content, and contact information such as phone numbers or a reply-to email address.</p>
+<p>Users that repeatedly infringe copyrights will have their account removed. Depending on the degree of the offense, their account may be removed immediately.</p>
+<h3>Privacy Policy</h3>
+<p>Privacy is a major concern for everyone. thunix respects the privacy rights of others &amp; refuses to collect/store/distribute anyone's private information, except when necessary; thunix collects usernames &amp; encrypted passwords for account registration purposes only, while logging IP addresses for server administration &amp; policy enforcement purposes. The privacy rights of individual users may be overturned ONLY when it is known that they have violated the service terms; this is done strictly to uphold the service terms as well as the law.</p>
 </p>
 <?php include 'FOOTER.php';?>
