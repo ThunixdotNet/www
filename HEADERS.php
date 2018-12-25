1261aa83fa1507fa6f9b76d41e655cbcbb1d6ab2
Anton McClure <amcclure@ttm.sh>
2018-12-25 13:50:03 -0500

    Update 'HEADERS.php'

--- HEADERS.php
+++ HEADERS.php
@@ -7,8 +7,8 @@
 <meta name="keywords" content="THUNIX,Shell,Provider,And,Web,Host,UNIX,Linux,HTML,CSS,XML,JavaScript,Gnu,Ubuntu,Debian,Arch,Mirrors,Mirror,Forums,Tildeverse">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-<link href="/server/site.css" rel="stylesheet" type="text/css" media="all">
-<script src="/server/site.js"></script>
-<link href="/server/favicon.png" rel="icon">
+<link href="/~amcclure/server/site.css" rel="stylesheet" type="text/css" media="all">
+<script src="/~amcclure/server/site.js"></script>
+<link href="/~amcclure/server/favicon.png" rel="icon">
 </head>
 <body>
