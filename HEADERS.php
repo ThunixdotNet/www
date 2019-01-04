c13afff1ef3b422e8582eaf5f7f299cc7b1b6592
Anton McClure <amcclure@ttm.sh>
2019-01-04 11:46:53 -0500

    Update 'HEADERS.php'

--- HEADERS.php
+++ HEADERS.php
@@ -2,9 +2,9 @@
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
 <head>
 <link rev="made" href="mailto:amcclure@ttm.sh">
-<meta name="description" content="Thunix Shell Provider and Web Host">
+<meta name="description" content="Thunix Shell Provider and Web Host & Thunix Phoenix Project">
 <meta name="author" content="Anton McClure">
-<meta name="keywords" content="THUNIX,Shell,Provider,And,Web,Host,UNIX,Linux,HTML,CSS,XML,JavaScript,Gnu,Ubuntu,Debian,Arch,Mirrors,Mirror,Forums,Tildeverse">
+<meta name="keywords" content="THUNIX,Shell,Provider,And,Web,Host,Phoenix,UNIX,Linux,HTML,CSS,XML,JavaScript,Gnu,Ubuntu,Debian,Arch,Mirrors,Mirror,Forums,Tildeverse">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
 <link href="/~amcclure/server/site.css" rel="stylesheet" type="text/css" media="all">
