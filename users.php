f841ab938eba741e52dde475e69fee23af609810
Anton McClure <amcclure@ttm.sh>
2019-01-05 17:41:21 -0500

    Update 'users.php'

--- users.php
+++ users.php
@@ -10,25 +10,26 @@
 <h3>Phoenix</h3>
 <ul>
 <!--<li><a href="//phoenix.thunix.cf/~/"></a></li>-->
-<li><a href="//phoenix.thunix.cf/~amcclure/">amcclure</a></li>
-<li><a href="//phoenix.thunix.cf/~ubergeek/">ubergeek</a></li>
-<li><a href="//phoenix.thunix.cf/~naglfar/">naglfar</a></li>
-<li><a href="//phoenix.thunix.cf/~parik/">parik</a></li>
-<li><a href="//phoenix.thunix.cf/~fosslinux/">fosslinux</a></li>
-<li><a href="//phoenix.thunix.cf/~slip/">slip</a></li>
-<li><a href="//phoenix.thunix.cf/~quetzalcoatl/">quetzalcoatl</a></li>
-<li><a href="//phoenix.thunix.cf/~redhat/">redhat</a></li>
-<li><a href="//phoenix.thunix.cf/~gokce/">gokce</a></li>
-<li><a href="//phoenix.thunix.cf/~r/">r</a></li>
-<li><a href="//phoenix.thunix.cf/~brendantcc/">brendantcc</a></li>
-<li><a href="//phoenix.thunix.cf/~dragon/">dragon</a></li>
-<li><a href="//phoenix.thunix.cf/~spanmer/">spanmer</a></li>
-<li><a href="//phoenix.thunix.cf/~panzer/">panzer</a></li>
-<li><a href="//phoenix.thunix.cf/~hawaii/">hawaii</a></li>
-<li><a href="//phoenix.thunix.cf/~trip/">trip</a></li>
-<li><a href="//phoenix.thunix.cf/~xenialito69/">xenialito69</a></li>
-<li><a href="//phoenix.thunix.cf/~freuddy/">freuddy</a></li>
-<li><a href="//phoenix.thunix.cf/~ringo/">ringo</a></li>
+  <li><a href="//phoenix.thunix.cf/~amcclure/">amcclure</a></li>
+  <li><a href="//phoenix.thunix.cf/~ubergeek/">ubergeek</a></li>
+  <li><a href="//phoenix.thunix.cf/~naglfar/">naglfar</a></li>
+  <li><a href="//phoenix.thunix.cf/~parik/">parik</a></li>
+  <li><a href="//phoenix.thunix.cf/~fosslinux/">fosslinux</a></li>
+  <li><a href="//phoenix.thunix.cf/~slip/">slip</a></li>
+  <li><a href="//phoenix.thunix.cf/~quetzalcoatl/">quetzalcoatl</a></li>
+  <li><a href="//phoenix.thunix.cf/~redhat/">redhat</a></li>
+  <li><a href="//phoenix.thunix.cf/~gokce/">gokce</a></li>
+  <li><a href="//phoenix.thunix.cf/~r/">r</a></li>
+  <li><a href="//phoenix.thunix.cf/~brendantcc/">brendantcc</a></li>
+  <li><a href="//phoenix.thunix.cf/~dragon/">dragon</a></li>
+  <li><a href="//phoenix.thunix.cf/~spanmer/">spanmer</a></li>
+  <li><a href="//phoenix.thunix.cf/~panzer/">panzer</a></li>
+  <li><a href="//phoenix.thunix.cf/~hawaii/">hawaii</a></li>
+  <li><a href="//phoenix.thunix.cf/~trip/">trip</a></li>
+  <li><a href="//phoenix.thunix.cf/~xenialito69/">xenialito69</a></li>
+  <li><a href="//phoenix.thunix.cf/~freuddy/">freuddy</a></li>
+  <li><a href="//phoenix.thunix.cf/~ringo/">ringo</a></li>
+  <li><a href="//phoenix.thunix.cf/~hexhaxtron/">hexhaxtron</a></li>
 </ul>
 
 <p>Note that most content on thunix are provided by thunix's users, not the owner of
