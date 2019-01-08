c298e8bf71681d115680c4c2eeceb6226f161e6e
Anton McClure <amcclure@ttm.sh>
2019-01-08 17:12:21 -0500

    Update 'users.php'

--- users.php
+++ users.php
@@ -1,42 +1,42 @@
 <?php include 'HEADER.php';?>
 <title>User Web Directories - thunix Shell Provider and Web Host</title>
 <h2>User Web Directories</h2>
+<!-- Leave this section for now.
 <h3>Summit</h3>
 <ul>
 <li><a href="/~amcclure/">Anton McClure</a> (amcclure)</li>
 <li><a href="/~ubergeek/">~Ubergeek</a> (ubergeek)</li>
 </ul>
-
+-->
 <h3>Phoenix</h3>
 <ul>
 <!--<li><a href="//phoenix.thunix.cf/~/"></a></li>-->
-  <li><a href="//phoenix.thunix.cf/~amcclure/">amcclure</a></li>
-  <li><a href="//phoenix.thunix.cf/~ubergeek/">ubergeek</a></li>
-  <li><a href="//phoenix.thunix.cf/~naglfar/">naglfar</a></li>
-  <li><a href="//phoenix.thunix.cf/~parik/">parik</a></li>
-  <li><a href="//phoenix.thunix.cf/~fosslinux/">fosslinux</a></li>
-  <li><a href="//phoenix.thunix.cf/~slip/">slip</a></li>
-  <li><a href="//phoenix.thunix.cf/~quetzalcoatl/">quetzalcoatl</a></li>
-  <li><a href="//phoenix.thunix.cf/~redhat/">redhat</a></li>
-  <li><a href="//phoenix.thunix.cf/~gokce/">gokce</a></li>
-  <li><a href="//phoenix.thunix.cf/~r/">r</a></li>
-  <li><a href="//phoenix.thunix.cf/~brendantcc/">brendantcc</a></li>
-  <li><a href="//phoenix.thunix.cf/~dragon/">dragon</a></li>
-  <li><a href="//phoenix.thunix.cf/~spanmer/">spanmer</a></li>
-  <li><a href="//phoenix.thunix.cf/~panzer/">panzer</a></li>
-  <li><a href="//phoenix.thunix.cf/~hawaii/">hawaii</a></li>
-  <li><a href="//phoenix.thunix.cf/~trip/">trip</a></li>
-  <li><a href="//phoenix.thunix.cf/~xenialito69/">xenialito69</a></li>
-  <li><a href="//phoenix.thunix.cf/~freuddy/">freuddy</a></li>
-  <li><a href="//phoenix.thunix.cf/~ringo/">ringo</a></li>
-  <li><a href="//phoenix.thunix.cf/~hexhaxtron/">hexhaxtron</a></li>
+  <li><a href="/~amcclure/">amcclure</a></li>
+  <li><a href="/~ubergeek/">ubergeek</a></li>
+  <li><a href="/~naglfar/">naglfar</a></li>
+  <li><a href="/~parik/">parik</a></li>
+  <li><a href="/~fosslinux/">fosslinux</a></li>
+  <li><a href="/~slip/">slip</a></li>
+  <li><a href="/~quetzalcoatl/">quetzalcoatl</a></li>
+  <li><a href="/~redhat/">redhat</a></li>
+  <li><a href="/~gokce/">gokce</a></li>
+  <li><a href="/~r/">r</a></li>
+  <li><a href="/~brendantcc/">brendantcc</a></li>
+  <li><a href="/~dragon/">dragon</a></li>
+  <li><a href="/~spanmer/">spanmer</a></li>
+  <li><a href="/~panzer/">panzer</a></li>
+  <li><a href="/~hawaii/">hawaii</a></li>
+  <li><a href="/~trip/">trip</a></li>
+  <li><a href="/~xenialito69/">xenialito69</a></li>
+  <li><a href="/~freuddy/">freuddy</a></li>
+  <li><a href="/~ringo/">ringo</a></li>
+  <li><a href="/~hexhaxtron/">hexhaxtron</a></li>
 </ul>
 
 <p>Note that most content on thunix are provided by thunix's users, not the owner of
 the server. Comments should be addressed to the owner of the web directory in the
 first instance - their email address here is
-<code>&lt;username&gt;@phoenix.thunix.cf</code> for Phoenix and
-<code>&lt;username&gt;@thunix.cf</code> for Summit
+<code>&lt;username&gt;@thunix.cf</code>,
 unless otherwise specified within their web directory.</p>
 <p>
 If there is a problem which is not resolved by
