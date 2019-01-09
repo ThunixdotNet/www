1ed868f7c158f5f73def6e80f1c4b49a82b665bd
Ubergeek <ubergeek@yourtilde.com>
2019-01-09 22:38:27 +0000

    Refactor HEADERS to use local version controlled copies of resources

--- HEADERS.php
+++ HEADERS.php
@@ -7,8 +7,8 @@
 <meta name="keywords" content="thunix,Shell,Provider,And,Web,Host,Phoenix,UNIX,Linux,HTML,CSS,XML,JavaScript,Gnu,Ubuntu,Debian,Arch,Mirrors,Mirror,Forums,Tildeverse">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-<link href="/~amcclure/server/site.css" rel="stylesheet" type="text/css" media="all">
-<script src="/~amcclure/server/site.js"></script>
-<link href="/~amcclure/server/favicon.png" rel="icon">
+<link href="/site.css" rel="stylesheet" type="text/css" media="all">
+<script src=/site.js"></script>
+<link href="/favicon.png" rel="icon">
 </head>
 <body>
