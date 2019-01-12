50c05805897198c0451e85bc4ff25c3da789b911
TechEmporium <techemporium.stlflops@gmail.com>
2019-01-12 15:47:35 -0500

    First site 2.0 copy

---
+++ donate.php
@@ -0,0 +1,29 @@
+<?php include 'HEADER.php'; ?>
+<title>Donations - thunix Shell Provider and Web Host</title>
+</head>
+<body>
+<div id="top">
+	<div id="header">
+		<div id="logo">
+			<h1>&#9763; thunix</h1>
+			<p>Shell Provider and Web Host</p>
+		</div>
+	</div>
+		<div id="page">
+			<div id="page-bgtop">
+				<div id="page-bgbtm">
+					<div id="content">
+						<div class="post">
+							<h2 class="title">Donations</h2>
+							<div style="clear: both;">&nbsp;</div>
+							<div class="entry">
+								<p>As this server and our projects are all a labor of love and goodwill for the community, operating the thunix server costs money. We love what we do and we love sharing what we do for free, but over time, operating expenses can have a big impact.</p>
+								<p>That being said, we also rely on the good nature of generous people, who are willing to donate to us. The price breakdown right now is &euro;38/month.  So to help with server costs and time spent, for now, you can send some BTC my (ubergeek's) way:</p>
+								<p style="text-align: center; color: #00FFFF;">1DFM3qY7XmCxGTFPATSQJVCNjvmWiVubGN</p>
+								<p>I'll set up a Patreon or Liberapay soonish.</p>
+							</div>
+						</div>
+					<div style="clear: both;">&nbsp;</div>
+					</div>
+<?php include 'MENU.php'; ?>
+<?php include 'FOOTER.php'; ?>
