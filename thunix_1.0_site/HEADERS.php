41bb08966d63a4fd02b88aaa36fdd897f371afb4
TechEmporium <techemporium.stlflops@gmail.com>
2019-01-12 16:06:10 -0500

    Original thunix 1.0 website, pre-modification

---
+++ thunix_1.0_site/HEADERS.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<head>
+<link rev="made" href="mailto:amcclure@ttm.sh">
+<meta name="description" content="thunix Shell Provider and Web Host & thunix Phoenix Project">
+<meta name="author" content="Anton McClure">
+<meta name="keywords" content="thunix,Shell,Provider,And,Web,Host,Phoenix,UNIX,Linux,HTML,CSS,XML,JavaScript,Gnu,Ubuntu,Debian,Arch,Mirrors,Mirror,Forums,Tildeverse">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<link href="/site.css" rel="stylesheet" type="text/css" media="all">
+<script src=/site.js"></script>
+<link href="/favicon.png" rel="icon">
+</head>
+<body>
