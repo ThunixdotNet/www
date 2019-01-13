611dc42592c67372c9e5280420c3014a1b37a8fe
ubergeek <ubergeek@yourtilde.com>
2019-01-13 12:52:03 -0500

    Update 'HEADER.php'

--- HEADER.php
+++ HEADER.php
@@ -2,8 +2,6 @@
 <html lang="en">
 <head>
 <meta name="description" content="thunix Shell Provider and Web Host & thunix Phoenix Project">
-<meta name="author" content="Anton McClure">
-<meta name="email" content="amcclure@ttm.sh">
 <meta name="keywords" content="thunix,Shell,Provider,And,Web,Host,Phoenix,UNIX,Linux,HTML,CSS,XML,JavaScript,Gnu,Ubuntu,Debian,Arch,Mirrors,Mirror,Forums,Tildeverse">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
