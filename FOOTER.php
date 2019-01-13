1856271e4aeed60639fee90ec7dde6370e705ec1
ubergeek <ubergeek@yourtilde.com>
2019-01-13 12:51:41 -0500

    Update 'FOOTER.php'

--- FOOTER.php
+++ FOOTER.php
@@ -1,9 +1,7 @@
 <div id="footer">
-<p>Copyright &copy; 2018 <a href="/~amcclure/">Anton McClure</a> <a href="mailto:amcclure@ttm.sh">&lt;amcclure@ttm.sh&gt;</a><br>
-Copyright &copy; 2018 <a href="/~ubergeek/">Ubergeek</a> <a href="mailto:ubergeek@yourtilde.com">&lt;ubergeek@yourtilde.com&gt;</a><br>
 This page is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by-nc-nd/4.0/">CC BY-NC-ND 4.0 license</a>. Permissions beyond the scope of this license may be available at <a href="/copyright.php">https://www.thunix.cf/copyright.php</a>.<br>
 <?php echo "Page last updated on " . date ("Y-m-d\TH:i:sO.", getlastmod());?><br>
 You may have to refresh the page to see any changes or updates.<br>
-All questions, comments, and concerns about this site should be emailed to <a href="mailto:amcclure@ttm.sh">amcclure@ttm.sh</a>.</p>
+All questions, comments, and concerns about this site should be emailed to <a href="mailto:root@thunix.cf">the amdinistration team</a>.</p>
 <p>Created with valid <a href="http://validator.w3.org/check?uri=referer">HTML</a> and <a href="http://jigsaw.w3.org/css-validator/check/referer">CSS</a> code.</p>
 </div></body></html>
