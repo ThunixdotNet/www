b451ed201430afd2726e896ba65fef8f7fb8b8a7
Ubergeek <ubergeek@yourtilde.com>
2019-01-27 19:47:29 +0000

    Fixed typo in footer

--- FOOTER.php
+++ FOOTER.php
@@ -2,6 +2,6 @@
 <p>This page is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by-nc-nd/4.0/">CC BY-NC-ND 4.0 license</a>. Permissions beyond the scope of this license may be available at <a href="/copyright.php">https://www.thunix.cf/copyright.php</a>.<br>
 <?php echo "Page last updated on " . date ("Y-m-d\TH:i:sO.", getlastmod());?><br>
 You may have to refresh the page to see any changes or updates.<br>
-All questions, comments, and concerns about this site should be sent to <a href="/contact.php">the amdinistration team</a>.</p>
+All questions, comments, and concerns about this site should be sent to <a href="/contact.php">the administration team</a>.</p>
 <p>Created with valid <a href="http://validator.w3.org/check?uri=referer">HTML</a> and <a href="http://jigsaw.w3.org/css-validator/check/referer">CSS</a> code.</p>
 </div></body></html>
