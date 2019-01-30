42327f265fc27110bb5bd16d6d197525d30777d1
TechEmporium <techemporium.stlflops@gmail.com>
2019-01-29 19:49:16 -0500

    Edited faq.php with better instructions on how to sign up

--- faq.php
+++ faq.php
@@ -16,7 +16,7 @@
 						<h2 class="title">Frequently Asked Questions</h2>
 						<div style="clear: both;">&nbsp;</div>
 						<div class="entry">
-							<p><b style="color: #FF00FF">How do I sign up for an account?</b><br>You can ask in #thunix on tilde.chat, or you can email amcclure (with the instructions on the main page).</p>
+							<p><b style="color: #FF00FF">How do I sign up for an account?</b><br>Simply by going to our <a href="/signup.php">signup page</a> and filling in the form. You can ask for help in #thunix on tilde.chat, or you can <a href="/contact.php>contact us</a>, if you run into any difficulties.</p>
 							<p><b style="color: #FF00FF">Who is running thunix?</b><br>The current system administrators are <a href="/~amcclure">amcclure</a> and <a href="/~ubergeek">ubergeek</a>.</p>
 							<p><b style="color: #FF00FF">What happened to the old thunix?  Why the name change?</b><br>The original machine and founder dissappeared without any warning to anyone, including server staff. For this reason, most things were not backed up, and we needed to obtain a new domain name, and a new set of machines.</p>
 							<p><b style="color: #FF00FF">I want a new package installed, or I want something changed on Thunix!</b></br>Excellent!  We're looking to make this system useful for the community!  You can submit a PR or an issue <a href="https://tildegit.org/thunix/ansible">here</a> to request the system change.</p>
