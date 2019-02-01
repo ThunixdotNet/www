0aeea38113fc210da0974925c81ef113c03ace72
Ubergeek <ubergeek@yourtilde.com>
2019-02-01 18:21:05 +0000

    Added paypal button

--- donate.php
+++ donate.php
@@ -21,6 +21,15 @@
 								<p style="text-align: center; color: #00FFFF;">1DFM3qY7XmCxGTFPATSQJVCNjvmWiVubGN</p>
 								<p>You can donate via Liberapay here:</p>
 								<p><script src="https://liberapay.com/ub3g33k/widgets/button.js"></script><noscript><a href="https://liberapay.com/ub3g33k/donate"><img alt="Donate using Liberapay" src="https://liberapay.com/assets/widgets/donate.svg"></a></noscript></p>
+								<p>Or via Paypal:</a>
+									<form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
+									<input type="hidden" name="cmd" value="_donations" />
+									<input type="hidden" name="business" value="GW2H85HY9VJ3L" />
+									<input type="hidden" name="currency_code" value="USD" />
+									<input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donateCC_LG.gif" border="0" name="submit" title="PayPal - The safer, easier way to pay online!" alt="Donate with PayPal button" />
+									<img alt="" border="0" src="https://www.paypal.com/en_US/i/scr/pixel.gif" width="1" height="1" />
+									</form>
+								</p>
 							</div>
 						</div>
 					<div style="clear: both;">&nbsp;</div>
