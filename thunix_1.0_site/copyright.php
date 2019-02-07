7a0ebc15a6c589d116210f014f6fa6d9374be42b
TechEmporium <techemporium.stlflops@gmail.com>
2019-02-06 21:47:18 -0500

    Changed hyperlink colours to be more readable

--- thunix_1.0_site/copyright.php
+++
@@ -1,5 +0,0 @@
-<?php include 'HEADER.php';?>
-<title>Copyright Notices and Acknowledgements - thunix Shell Provider and Web Host</title>
-<h2>Copyright Notices and Acknowledgements</h2>
-<p>(Coming soon)</p>
-<?php include 'FOOTER.php';?>
--- thunix_1.0_site/donate.php
+++
@@ -1,8 +0,0 @@
-<?php include 'HEADER.php';?>
-<title> - thunix Shell Provider and Web Host</title>
-<h2>Donations</h2>
-<p>This server costs money to operate.  It's also operated as a labor of love and goodwill for the community</p>
-<p>That being said, the price breakdown right now is 38Euro/month.  So help with server costs, and time spent, for now, you can send some BTC my (ubergeek's) way</p>
-<p>1DFM3qY7XmCxGTFPATSQJVCNjvmWiVubGN</p>
-<p>I'll set up a Patreon or Liberapay soonish.</p>
-<?php include 'FOOTER.php';?>
