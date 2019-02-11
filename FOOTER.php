b12e03079f405df295e779b53b900c207db0b287
Anton McClure <amcclure@ttm.sh>
2019-02-10 22:12:21 -0500

    Changed *.thunix.cf links to *.thunix.net

--- FOOTER.php
+++ FOOTER.php
@@ -1,5 +1,5 @@
 <div id="footer">
-<p>This page is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by-nc-nd/4.0/">CC BY-NC-ND 4.0 license</a>. Permissions beyond the scope of this license may be available at <a href="/copyright.php">https://www.thunix.cf/copyright.php</a>.<br>
+<p>This page is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by-nc-nd/4.0/">CC BY-NC-ND 4.0 license</a>. Permissions beyond the scope of this license may be available at <a href="/copyright.php">https://www.thunix.net/copyright.php</a>.<br>
 <?php echo "Page last updated on " . date ("Y-m-d\TH:i:sO.", getlastmod());?><br>
 You may have to refresh the page to see any changes or updates.<br>
 All questions, comments, and concerns about this site should be sent to <a href="/contact.php">the administration team</a>.</p>
