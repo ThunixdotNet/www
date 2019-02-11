488c6b8aede6a7409c325aa10057792f825b5cfb
Anton McClure <amcclure@ttm.sh>
2019-02-10 22:17:20 -0500

    Changed *.thunix.cf links to *.thunix.net

--- users.php
+++ users.php
@@ -26,7 +26,7 @@
 									<li><a href="/~<?=$user?>/">~<?=$user?></a></li>
 									<?php endforeach; ?>
 									</ul>
-								<p>Note that most content on thunix are provided by thunix's users, not the owner of the server. Comments should be addressed to the owner of the web directory in the first instance - their email address here is <code>&lt;username&gt;@thunix.cf</code>, unless otherwise specified within their web directory.</p>
+								<p>Note that most content on thunix are provided by thunix's users, not the owner of the server. Comments should be addressed to the owner of the web directory in the first instance - their email address here is <code>&lt;username&gt;@thunix.net</code>, unless otherwise specified within their web directory.</p>
 								<p>If there is a problem which is not resolved by the owner of the web directory, please send us an <a href="/abuse.php">abuse report</a>.</p>
 								<p>If you want to be added to this list, simply create an index.html or index.php file and upload it to your public_html directory. If you want to be removed from the list, simply remove your index file from your public_html directory.</p>
 							</div>
