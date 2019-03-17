869ecb58936119178b29d5661fef45ca936cbb19
Anton McClure <amcclure@thunix.net>
2019-03-17 19:54:16 -0400

    Updated ad code

--- FOOTER.php
+++ FOOTER.php
@@ -5,8 +5,9 @@ You may have to refresh the page to see any changes or updates.<br>
 All questions, comments, and concerns about this site should be sent to <a href="/contact.php">the administration team</a>.</p>
 <p>Created with valid <a href="http://validator.w3.org/check?uri=referer">HTML</a> and <a href="http://jigsaw.w3.org/css-validator/check/referer">CSS</a> code.</p>
 <!-- EasyBanner code begin -->
-<P><CENTER>
-<IFRAME SRC="https://banner.tildeverse.org/work.php?ID=thunix" width=468 height=60 marginwidth=0 marginheight=0 hspace=0 vspace=0 frameborder=0 scrolling="no"></IFRAME>
-</CENTER></P>
+<CENTER>
+<IFRAME SRC="https://banner.tildeverse.org/work.php?ID=thunix" width=468 height=60 marginwidth=0 marginheight=0 hspace=0 vspace=0 frameborder=1 scrolling="no"></IFRAME><br />
+Ads by <a href="//banner.tildeverse.org">Tildeverse Banner Exchange</a>
+</CENTER>
 <!-- EasyBanner code end -->
 </div></body></html>
