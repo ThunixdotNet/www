b9d5be7531eb94d63ac069ec96bda264f2503874
Anton McClure <amcclure@thunix.net>
2019-03-28 11:08:29 -0400

    Updated Terms of Service to fix loopholes

--- tos.php
+++ tos.php
@@ -16,7 +16,7 @@
 							<h2 class="title">Terms of Service</h2>
 							<div style="clear: both;">&nbsp;</div>
 							<div class="entry">
-								<p>Nothing is without its rules and regulations; thunix is no exception. Below are the site's service terms. Everything in this page should be clear to everyone who wishes to use thunix's services; failure to abide by these terms can result in such penalties as service bans and even legal action, depending on the severity of a violation. Be sure to read this page carefully and understand it.</p><p>thunix relies on the common courtesy and consideration of users to make its services fair for everyone. The following offences will result in a service ban:</p>
+								<p>Nothing is without its rules and regulations; thunix is no exception. Below are the site's service terms. Everything in this page should be clear to everyone who wishes to use thunix's services; failure to abide by these terms can result in penalties such as service bans and may result in legal action against any offending users, depending on the severity of each case and any other individuals who may have been affected. Thunix may conduct investigations on any suspected violations, and we will cooperate with law enforcement agencies with their investigations. Be sure to read this page carefully and understand it.</p>
 								<ol>
 								<li>Deliberately defacing the accounts of other users</li>
 								<li>Deliberately trying to disrupt thunix's server</li>
@@ -28,13 +28,13 @@
 								<li>Storing/distributing, promoting, or encouraging use of pirated/cracked software, license keys, license/registration circumvention programs, or any questionable programs/scripts that can risk causing unauthorized modifications, or disrupts network services for any programs or devices.</li>
 								<li>Mining cryptocurrencies</li>
 								<li>Spamming on any forums, mailing lists, irc channels, newsgroups, etc.</li>
+                                <li>Leaking or publishing any user's or individual's personal information without that person's consent.</li>
 								</ol>
-								<p>Items 4, 5, 6, 7, and 8 may also result in legal action against any offending users, depending on the severity of each case and any other individuals who may have been affected.</p>
-								<p>Copyright infringement is not allowed on thunix, and we will not allow any illegal content to be distributed.</p>
-								<p>It's also expected that all users respect the copyrights of those who produce original content of any kind (only share if the author or license grants you permission). However, using accounts for external file storage is permissible, under fair use and other laws promoting consumer rights. Note that original content from other users must also be respected, though creating/sharing public domain content is highly encouraged.</p>
+								<p>Copyright infringement is not allowed on thunix, and we will not allow any illegal content to be distributed. It's also expected that all users respect the copyrights of those who produce original content of any kind and only share if the author or license grants you permission. Content hosted on accounts used for external file storage must also follow copyright law.</p>
 								<p>If you want to request for content you own the copyright for to be removed from thunix, please refer to our <a href="/abuse.php">abuse reporting page</a>. In the email, please identify who you are, if you are the copyright holder or legally representing them, the exact files you want removed with links to the files and infringed content, and contact information such as phone numbers or a reply-to email address.</p>
-								<p>Users that repeatedly infringe copyrights will have their account removed. Depending on the degree of the offense, their account may be removed immediately.</p>
-                                <p>These rules also apply to official communication services such as forums, mailing lists, irc channels, newsgroups, and any other service hosted on thunix. If you come across anything that violates the terms of service, please let us know with the <a href="/abuse.php">abuse reporting page</a>.</p>
+								<p>Users that repeatedly violate the Terms of Service will have their account removed. Depending on the degree of the offense, their account may be removed immediately. These terms also apply to communication services such as forums, mailing lists, irc channels, newsgroups, and any other service either hosted on or used for thunix. If you come across anything that violates the terms of service, please let us know with the <a href="/abuse.php">abuse reporting page</a>.</p>
+                                <hr>
+                                <p>Last Updated: March 28, 2019</p>
 							</div>
 						</div>
 						<div style="clear: both;">&nbsp;</div>
