609ef4febda845395ec9afda49266aec0fd0bf1a
Anton McClure <amcclure@thunix.net>
2019-05-18 12:47:21 -0400

    added menu links, temporarily commented out unused section

--- MENU.php
+++ MENU.php
@@ -13,6 +13,8 @@
 	<li><a href="contact.php">Contact Us</a></li>
 	<li><a href="signup.php">Sign Up</a></li>
 	<li><a href="abuse.php">Report Abuse</a></li>
+        <li><a href="//ftp.thunix.net/">thunix Mirror Services</a></li>
+        <li><a href="/~amcclure/stats/">Web Server Stats</a></li>
 	<li><a href="donate.php">Donations</a></li>
 	</ul>
 	</li>
@@ -33,13 +35,13 @@
 	<li><a href="/~naglfar/">Naglfar</a></li>
 	<li><a href="/~fosslinux/">fosslinux</a></li>
 	</ul>
-	<li>
+<!--	<li>
 	<h2>Other Information</h2>
 	<ul>
 	<li><a href=".">(Coming soon)</a></li>
 	</ul>
 	</li>
-	</ul>
+	</ul>-->
 </div>
 <div style="clear: both;">&nbsp;</div>
 </div></div></div></div>
