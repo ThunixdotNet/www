a8d662db12e65e82f9bae91e17bed23584382d81
Ubergeek <ubergeek@yourtilde.com>
2019-05-28 14:01:39 +0000

    Update tilde.json, and updated list of admins on FAQ

--- faq.php
+++ faq.php
@@ -6,7 +6,7 @@
 						<div style="clear: both;">&nbsp;</div>
 						<div class="entry">
 							<p><b style="color: #FF00FF">How do I sign up for an account?</b><br>Simply by going to our <a href="/signup.php">signup page</a> and filling in the form. You can ask for help in #thunix on tilde.chat, or you can <a href="/contact.php">contact us</a>, if you run into any difficulties.</p>
-							<p><b style="color: #FF00FF">Who is running thunix?</b><br>The current system administrators are <a href="/~amcclure">amcclure</a> and <a href="/~ubergeek">ubergeek</a>.</p>
+							<p><b style="color: #FF00FF">Who is running thunix?</b><br>The current system administrators are <a href="/~amcclure">amcclure</a>, <a href="/~ubergeek">ubergeek</a>, <a href="/~naglfar">Naglfar</a>, and <a href="/~fosslinux">fosslinux</a>.</p>
 							<p><b style="color: #FF00FF">What happened to the old thunix?  Why the name change?</b><br>The original machine and founder dissappeared without any warning to anyone, including server staff. For this reason, most things were not backed up, and we needed to obtain a new domain name, and a new set of machines.</p>
 							<p><b style="color: #FF00FF">I want a new package installed, or I want something changed on Thunix!</b></br>Excellent!  We're looking to make this system useful for the community!  You can submit a PR or an issue <a href="https://tildegit.org/thunix/ansible">here</a> to request the system change.</p>
 							<p><b style="color: #FF00FF">Can I get password-based login?  Old thunix had it!</b></br>No.  Sorry.  Not for shell access.  For other integrated services, password auth will be enabled, but not for your ssh connection.  We use key based authentication, as it's more secure, and more convienent for you, to be honest.</p>
