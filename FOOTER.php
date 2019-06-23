c985d89b0584fdef3cb3f4f5ab1b33e03ed421db
TechEmporium <techemporium.stlflops@gmail.com>
2019-06-23 00:38:34 -0400

    Repaired W3C validator referer links

--- FOOTER.php
+++ FOOTER.php
@@ -3,7 +3,7 @@
 <?php echo "Page last updated on " . date ("Y-m-d\TH:i:sO.", getlastmod());?><br>
 You may have to refresh the page to see any changes or updates.<br>
 All questions, comments, and concerns about this site should be sent to <a href="/contact.php">the administration team</a>.</p>
-<p>Created with valid <a href="http://validator.w3.org/check?uri=referer">HTML</a> and <a href="http://jigsaw.w3.org/css-validator/check/referer">CSS</a> code.</p>
+<p>Created with valid <a href="https://validator.w3.org/check?uri=referer">HTML</a> and <a href="https://jigsaw.w3.org/css-validator/check/referer">CSS</a> code.</p>
 <!-- EasyBanner code begin -->
 <!--<CENTER>
 <IFRAME SRC="https://banner.tildeverse.org/work.php?ID=thunix" width=468 height=60 marginwidth=0 marginheight=0 hspace=0 vspace=0 frameborder=1 scrolling="no"></IFRAME><br />
