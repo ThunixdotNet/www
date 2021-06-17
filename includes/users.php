99b9d67af0b15b325a7b999ee3fda0926f474992
Naglfar <naglfar@tilde.team>
2021-06-17 16:26:44 +0200

    Execute statement if expression evaluates to true

--- includes/users.php
+++ includes/users.php
@@ -2,10 +2,11 @@
 print "<!-- Begin autogen userdir list -->";
 print "<ul style='list-style: none; margin-left: -40px;'>";
 foreach (glob("/home/*") as $user):
-		if (!is_dir($user . "/public_html") || (!file_exists($user . "/public_html/index.html") && !file_exists($user . "/public_html/index.php")))
-	continue;
-	$user = basename($user);
-	print"<li><a href='$site_root/~$user/'>~$user</a></li>";
+	if (is_dir($user . "/public_html") && (file_exists($user . "/public_html/index.html") || file_exists($user . "/public_html/index.php")))
+	{
+		$user = basename($user);
+		print"<li><a href='$site_root/~$user/'>~$user</a></li>";
+	}
 endforeach;
 print "</ul></div>
 <!-- End Autgen userdir list -->";
