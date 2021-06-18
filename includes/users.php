73f0a980d9905d0756af9b4af55fc8d825ae4831
Naglfar <naglfar@tilde.team>
2021-06-18 10:50:31 +0200

    Do not list empty directory

--- includes/users.php
+++ includes/users.php
@@ -4,6 +4,7 @@ print "<ul style='list-style: none; margin-left: -40px;'>";
 foreach (glob("/home/*") as $user):
 	if (is_dir($user . "/public_html"))
 	if (!file_exists($user . "/public_html/coming_soon"))
+	if (count(scandir($user."/public_html")) != 2)
 	{
 		$user = basename($user);
 		print"<li><a href='$site_root/~$user/'>~$user</a></li>";
