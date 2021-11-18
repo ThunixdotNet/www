b8545fb767748d696fe207abc27322cc1111ceee
Naglfar <naglfar@tilde.team>
2021-11-18 17:13:06 +0100

    There is an index.html already

--- includes/users.php
+++ includes/users.php
@@ -4,14 +4,13 @@ print "<!-- Begin autogen userdir list -->";
 print "<ul style='list-style: none; margin-left: -40px;'>";
 foreach (glob("/home/*") as $userpath):
     if (is_dir("$userpath/public_html"))
-	if (count(scandir("$userpath/public_html")) != 2)
-	{
-		$user = basename($userpath);
-		if(sha1_file($html_skel) == sha1_file("$userpath/public_html/index.html"))
-		  print"<li>~$user</li>\n";
-		else
-		  print"<li><a href='$site_root/~$user/'>~$user</a></li>\n";
-	}
+    {
+	$user = basename($userpath);
+	if(sha1_file($html_skel) == sha1_file("$userpath/public_html/index.html"))
+	  print"<li>~$user</li>\n";
+	else
+	  print"<li><a href='$site_root/~$user/'>~$user</a></li>\n";
+    }
 endforeach;
 print "</ul></div>
 <!-- End Autgen userdir list -->";
