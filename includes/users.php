2ae6c8c2b80ac37b5063a55e8c3e5a6cbff9360c
deepend-tildeclub <58404188+deepend-tildeclub@users.noreply.github.com>
2025-04-26 13:17:26 -0600

    Update users.php

--- includes/users.php
+++ includes/users.php
@@ -7,13 +7,16 @@ print "<ul style='list-style: none; margin-left: -40px;'>";
 foreach (glob("/home/*") as $userpath) {
     if (is_dir("$userpath/public_html")) {
         $user = basename($userpath);
-        
+
+        // Use @ to suppress warnings in case the user directory/files are not readable.
         $skeletonMatch = (@sha1_file($html_skel) === @sha1_file("$userpath/public_html/index.html"));
         $isEmptyPubhtml = (@count(@scandir("$userpath/public_html")) === 2); // 2 => "." and ".."
 
         if ($skeletonMatch || $isEmptyPubhtml) {
+            // If it matches the skeleton index.html or is empty, display without a link
             print "<li>~$user</li>\n";
         } else {
+            // Otherwise, link to the user's directory
             print "<li><a href='$site_root/~$user/'>~$user</a></li>\n";
         }
     }
